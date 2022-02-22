46b1578d37d53613b3256120ca1547a8a2d3e61c
rasmuswoldike <rswoldike@gmail.com>
2022-02-22 13:22:55 +0100

    Initial commit

---
+++ src/main/java/com/example/htmlogcss/HtmLogCssApplication.java
@@ -0,0 +1,13 @@
+package com.example.htmlogcss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmLogCssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HtmLogCssApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/htmlogcss/HtmLogCssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.htmlogcss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmLogCssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
